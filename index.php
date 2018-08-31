fbb98bd57bc2df86d7452340e7e1f7a28ba2356f
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-08-31 13:11:33 +0200

    ajout d'un boutton reset

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php
+//defintion de la regex qui va controler que les saisies ne comportent que des caractères décimaux
+$numberPattern = '#^(\d+\.\d+)|(\d+)$#';
 //définition de la variable affichant le résutat
-$numberPattern = '#^[0-9]+.[0-9]+$#';
 $result = 0;
+// conditions pour la réinitialisation du formulaire
+if (!empty($_POST['reset'])) {
+    $reset = $_POST['reset'];
+
+    if (($reset == true)) {
+        $_POST['number1'] = ' ';
+        $_POST['number2'] = ' ';
+        $result = 0;
+    }
+}
 
 //association des variables number aux 2 chiffres saisis si le formulaire à bien été envoyé et si les valeurs sont présentes
-if (!empty($_POST['number1'])) {
-    $number1 = $_POST['number1'];
-    //vérification si $number1 est bien un chiffre
-    if (!preg_match($numberPattern, $number1)) {
-        $result = 'Utilisez uniquement des caractères décimaux pour le premier chiffre';
+    if (!empty($_POST['number1'])) {
+        $number1 = $_POST['number1'];
+    } else { //si valeur 1 est absente
+        ?>
+        <?php $result = 'Veuillez saisir un premier nombre'; ?>
+        <?php
     }
-} else { //si valeur 1 est absente
-    $result = 'Veuillez saisir un premier nombre';
-}
-if (!empty($_POST['number2'])) {
-    $number2 = $_POST['number2'];
-    //vérification si $number2 est bien un chiffre
-    if (!preg_match($numberPattern, $number2)) {
-        $result = 'Utilisez uniquement des caractères décimaux pour le deuxième chiffre';
+    if (!empty($_POST['number2'])) {
+        $number2 = $_POST['number2'];
+    } else { // si valeur 2 est absente 
+        ?>
+        <?php $result = 'veuillez saisir le deuxième nombre'; ?>
+        <?php
     }
-} else { // si valeur 2 est absente 
-    $result = 'veuillez saisir le deuxième nombre';
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,7 +36,7 @@ if (!empty($_POST['number2'])) {
         <meta charset="utf-8" />
         <title>Calculatrice</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css" />   
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"  />
         <link rel="stylesheet" href="assets/css/style.css" />
     </head>
     <body>
@@ -60,49 +67,59 @@ if (!empty($_POST['number2'])) {
                         <input class="btn waves-effect waves-light" type="submit" name="division" value="/" />
                     </div>
                 </div>
+                <div class="row" id="#">
+                    <div class="col s12 m12 l12 center-align" id="button">
+                        <input class="btn red darken-4 waves-effect waves-light" type="submit" name="reset" value="Reset" />
+                    </div>                    
+                </div>
             </form>
             <?php
             if ((!empty($_POST['number1'])) && (!empty($_POST['number2']))) {
-              if ((preg_match($numberPattern, $number1)) && (preg_match($numberPattern, $number2))) {
-                // conditions pour la division vérifiant la présence du signe en récupérant sa valeur dans l'input
-                if (!empty($_POST['division'])) {
-                    //définition de la variable d'opération en fonction de la valeur récupérée
-                    $division = $_POST['division'];
-                    // condition si le 2eme chiffre est égale à 0 => division impossible par 0
-                    if (($division == '/') && ($number2 == 0)) {
-                        $result = 'division impossible';
-                        //sinon opération possible
-                    } else {
-                        $result = $number1 / $number2;
+                if ((preg_match($numberPattern, $number1)) && (preg_match($numberPattern, $number2))) {
+                    // conditions pour la division vérifiant la présence du signe en récupérant sa valeur dans l'input
+                    if (!empty($_POST['division'])) {
+                        //définition de la variable d'opération en fonction de la valeur récupérée
+                        $division = $_POST['division'];
+                        // condition si le 2eme chiffre est égale à 0 => division impossible par 0
+                        if (($division == '/') && ($number2 == 0)) {
+                            $result = 'division impossible';
+                            //sinon opération possible
+                        } else {
+                            $result = $number1 / $number2;
+                        }
+                        // conditions pour la somme vérifiant la présence du signe en récupérant sa valeur dans l'input
+                    } elseif (!empty($_POST['sum'])) {
+                        //définition de la variable d'opération en fonction de la valeur récupérée
+                        $sum = $_POST['sum'];
+                        if ($sum == '+') {
+                            $result = $number1 + $number2;
+                        }
+                        // conditions pour la soustraction vérifiant la présence du signe en récupérant sa valeur dans l'input
+                    } elseif (!empty($_POST['substraction'])) {
+                        //définition de la variable d'opération en fonction de la valeur récupérée
+                        $substraction = $_POST['substraction'];
+                        if ($substraction == '-') {
+                            $result = $number1 - $number2;
+                        }
+                        // conditions pour la multiplication vérifiant la présence du signe en récupérant sa valeur dans l'input
+                    } elseif (!empty($_POST['multiplication'])) {
+                        //définition de la variable d'opération en fonction de la valeur récupérée
+                        $multiplication = $_POST['multiplication'];
+                        if ($multiplication == '*') {
+                            $result = $number1 * $number2;
+                        }
                     }
-                    // conditions pour la somme vérifiant la présence du signe en récupérant sa valeur dans l'input
-                } elseif (!empty($_POST['sum'])) {
-                    //définition de la variable d'opération en fonction de la valeur récupérée
-                    $sum = $_POST['sum'];
-                    if ($sum == '+') {
-                        $result = $number1 + $number2;
-                    }
-                    // conditions pour la soustraction vérifiant la présence du signe en récupérant sa valeur dans l'input
-                } elseif (!empty($_POST['substraction'])) {
-                    //définition de la variable d'opération en fonction de la valeur récupérée
-                    $substraction = $_POST['substraction'];
-                    if ($substraction == '-') {
-                        $result = $number1 - $number2;
-                    }
-                    // conditions pour la multiplication vérifiant la présence du signe en récupérant sa valeur dans l'input
-                } elseif (!empty($_POST['multiplication'])) {
-                    //définition de la variable d'opération en fonction de la valeur récupérée
-                    $multiplication = $_POST['multiplication'];
-                    if ($multiplication == '*') {
-                        $result = $number1 * $number2;
-                    }
+                } elseif ($reset == true) {
+                    $resultat = ' ';
+                } else {
+                
+                    ?>
+                    <span class="error"><?php $result = 'Veuillez utiliser des valeurs décimales'; ?></span>
+                    <?php
                 }
-            } else {
-                $result = 'Veuillez utiliser des valeurs décimales';
-            }
             }
             ?>
-            <p>Résultat : <?= (empty($_POST['number1']) && empty($_POST['number2'])) ? '' : $result // affichage du résultat après opération       ?> </p>
+            <p>Résultat : <?= (empty($_POST['number1']) && empty($_POST['number2'])) ? '' : $result // affichage du résultat après opération    ?> </p>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
